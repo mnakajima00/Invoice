218217fe43ceee36913a4c461cfbcb03c4b9cad8
Maiku <maiku00@my.yorku.ca>
2019-12-30 11:58:19 -0500

    Added basic JFrame

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,7 +1,22 @@
+import javax.swing.*;
+
 public class Invoice {
 
     public Invoice(){
 
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Invoice");
+
+            frame.setSize(500,500);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+
+    }
+
+    public static void main(String[] args){
+        Invoice invoice = new Invoice();
     }
 
 }
